c6ee8ca8ca1e0b77e9bc2735bec054a77e86083f
Zikrilloh <asralkhanov07@gmail.com>
2026-05-06 20:10:28 +0900

    fix: add CSRF truste origins for Railway

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,9 +4,13 @@ from decouple import config
 DEBUG = False
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
+
 # Railway handles SSL termination — don't redirect
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-45922.up.railway.app']
